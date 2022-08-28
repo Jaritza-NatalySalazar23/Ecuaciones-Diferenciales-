72cfb8c1ca76a7f556f2d838fddf8354445b72ac
Jaritza-NatalySalazar23 <111723432+Jaritza-NatalySalazar23@users.noreply.github.com>
2022-08-28 13:21:47 -0500

    Update codigo en MATLAB  ejercicio.m

--- codigo en MATLAB  ejercicio.m
+++ codigo en MATLAB  ejercicio.m
@@ -1,4 +1,4 @@
-%comprovar segundo ejercicio final
+%comprobar segundo ejercicio final
 %ingresamos los numeros correspondientes a la matriz 
 S =dsolve ('Dx=-3*x+1*y+3*t','Dy=2*x+-4*y+exp(-t)', 't');
 
