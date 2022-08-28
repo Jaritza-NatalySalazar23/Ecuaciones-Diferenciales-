35a0a6f379c677ee7cdf858cce4bd1585ac164c1
Jaritza-NatalySalazar23 <111723432+Jaritza-NatalySalazar23@users.noreply.github.com>
2022-08-28 13:10:42 -0500

    Add files via upload

---
+++ codigo en MATLAB  ejercicio.m
@@ -0,0 +1,12 @@
+%comprovar segundo ejercicio final
+%ingresamos los numeros correspondientes a la matriz 
+S =dsolve ('Dx=-3*x+1*y+3*t','Dy=2*x+-4*y+exp(-t)', 't');
+
+% obtenemos una respuesta S = x:[1x1 sym], y:[1x1 sym]
+
+%luego para obtener X =S.x, Y=S.y
+
+X=S.x
+Y=S.y
+simplify(X)
+simplify(Y)
